bfafff1313eb0016079c0111f7481fac2910ae73
Revisto <walt.shabani@gmail.com>
2021-03-04 22:45:18 +0330

    fix working with env file in models

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from mcstatus import MinecraftServer
 import requests
-
+import setting
 
 class Minecraft_Status:
     def get_online_users(self):
-        status_server = (requests.get("https://api.mcsrvstat.us/2/185.110.190.30:25566").json())
+        status_server = (requests.get(f"https://api.mcsrvstat.us/2/{setting.minecraft_server_address}").json())
         online_players = status_server["players"]["online"]
         return online_players
 
     def get_online_users_direct(self):
-        server = MinecraftServer.lookup("185.110.190.30:25566")
+        server = MinecraftServer.lookup(f"{setting.minecraft_server_address}")
         status = server.status()
         return status.players.online
