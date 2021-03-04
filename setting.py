cd45bf2c39fba4ccf587899c77ca2a960ba5e3b1
Revisto <walt.shabani@gmail.com>
2021-03-04 22:37:30 +0330

    add server address to setting.py

--- setting.py
+++ setting.py
@@ -6,3 +6,4 @@ dotenv.load_dotenv()
 minecraft_server_ip = getenv("minecraft_server_ip")
 minecraft_server_port = getenv("minecraft_server_port")
 telegram_access_token = getenv("telegram_robot_access_token")
+minecraft_server_address = f"{minecraft_server_ip}:{minecraft_server_port}"
