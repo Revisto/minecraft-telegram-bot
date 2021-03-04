08281ef06ac77c5a55c642880e8b1305294274fc
Revisto <walt.shabani@gmail.com>
2021-03-04 23:23:52 +0330

    add getting online players usernames feature to bot

--- bot.py
+++ bot.py
@@ -2,14 +2,18 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import setting
 from models import Minecraft_Status
 
-def players(update, context):
-    update.message.reply_text(f'There are {Minecraft_Status().get_online_users_direct()} online players in minecraft.')
+def number_of_online_players(update, context):
+    update.message.reply_text(f'There are {Minecraft_Status().get_online_users_count()} online players in minecraft.')
+
+def names_of_online_players(update, context):
+    update.message.reply_text(f"Online players' usernames in minecraft are {Minecraft_Status().get_online_users_names()}")
 
 def main():
     updater = Updater(setting.telegram_access_token, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("players", players))
+    dp.add_handler(CommandHandler("numberPlayers", number_of_online_players))
+    dp.add_handler(CommandHandler("Players", names_of_online_players))
 
     updater.start_polling()
     updater.idle()
