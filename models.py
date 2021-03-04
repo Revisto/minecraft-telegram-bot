795ce7e2a38ca4716b81c2f8863be0052b1612cb
Revisto <walt.shabani@gmail.com>
2021-03-04 23:19:58 +0330

    add function to get mc online users names

--- models.py
+++ models.py
@@ -3,12 +3,24 @@ import requests
 import setting
 
 class Minecraft_Status:
-    def get_online_users(self):
+    def __init__(self):
+        mc_server_lookup = MinecraftServer.lookup(f"{setting.minecraft_server_address}")
+        mc_server_status = mc_server_lookup.status()
+        mc_server_query = mc_server_lookup.query()
+        self.mc_server_status = mc_server_status
+        self.mc_server_query = mc_server_query
+
+    def get_online_users_count_via_api(self):
         status_server = (requests.get(f"https://api.mcsrvstat.us/2/{setting.minecraft_server_address}").json())
         online_players = status_server["players"]["online"]
         return online_players
 
-    def get_online_users_direct(self):
-        server = MinecraftServer.lookup(f"{setting.minecraft_server_address}")
-        status = server.status()
-        return status.players.online
+    def get_online_users_count(self):
+        return self.mc_server_status.players.online
+
+    def get_online_users_names(self):
+        online_users_names_list = self.mc_server_query.players.names
+        online_users_names = ", ".join(online_users_names_list)
+        return online_users_names if online_users_names != "" else None
+
+print (Minecraft_Status().get_online_users_names())
