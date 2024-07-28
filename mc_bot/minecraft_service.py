310ac7a473013c2dde3ab8afa8d5c67162a29765
Revisto <walt.shabani@gmail.com>
2024-07-28 22:16:56 +0330

    change minecraft checker service api from mcsrvstat to mcstatus

--- mc_bot/minecraft_service.py
+++ mc_bot/minecraft_service.py
@@ -1,13 +1,13 @@
 import requests
 from config import Config
 
-def error_handler(func):
+def offline_server_handler(func):
     def wrapper(self, *args, **kwargs):
         response_json = self._get_response_json()
-        if response_json.get("debug", {}).get("ping") is False:
+        if response_json.get("online") is False:
             return {
                 "status": "error",
-                "message": response_json.get("debug", {}).get("error", "Unknown error")
+                "message": "lol, the server's down."
             }
         return func(self, response_json, *args, **kwargs)
     return wrapper
@@ -17,18 +17,18 @@ class Minecraft_Status:
         self.MINECRAFT_SERVER_ADDRESS = Config.MINECRAFT_SERVER_ADDRESS
     
     def _get_response_json(self):
-        response = requests.get(f"https://api.mcsrvstat.us/3/{self.MINECRAFT_SERVER_ADDRESS}")
+        response = requests.get(f"https://api.mcstatus.io/v2/status/java/{self.MINECRAFT_SERVER_ADDRESS}")
         response_json = response.json()
         return response_json
 
-    @error_handler
+    @offline_server_handler
     def get_online_users_count(self, response_json):
         return {
             "status": "success",
             "online_users_count": response_json.get("players", {}).get("online", 0)
         }
 
-    @error_handler
+    @offline_server_handler
     def get_online_users_names(self, response_json):
         return {
             "status": "success",
